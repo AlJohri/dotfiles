497844faf50663ca599d81c38381bac584a8f9ef
Al Johri <al.johri@gmail.com>
2016-11-15 19:54:22 -0500

    Create shprofile.sh

---
+++ shprofile.sh
@@ -0,0 +1,31 @@
+# 077 would be more secure, but 022 is more useful.
+umask 022
+
+# Save more history
+export HISTSIZE=100000
+export SAVEHIST=100000
+
+# OS variables
+[ $(uname -s) = "Darwin" ] && export OSX=1 && export UNIX=1
+[ $(uname -s) = "Linux" ] && export LINUX=1 && export UNIX=1
+uname -s | grep -q "_NT-" && export WINDOWS=1
+
+# Fix systems missing $USER
+[ -z "$USER" ] && export USER=$(whoami)
+
+# Count CPUs for Make jobs
+if [ $OSX ]
+then
+  export CPUCOUNT=$(sysctl -n hw.ncpu)
+elif [ $LINUX ]
+then
+  export CPUCOUNT=$(getconf _NPROCESSORS_ONLN)
+else
+  export CPUCOUNT="1"
+fi
+
+if [ "$CPUCOUNT" -gt 1 ]
+then
+  export MAKEFLAGS="-j$CPUCOUNT"
+  export BUNDLE_JOBS="$CPUCOUNT"
+fi
