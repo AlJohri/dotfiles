a91065da74607b333801ced8343e2d7296262bf1
Al Johri <al.johri@gmail.com>
2016-11-15 20:25:11 -0500

    Update shrc.sh

--- shrc.sh
+++ shrc.sh
@@ -44,26 +44,13 @@ quiet_which() {
   which $1 &>/dev/null
 }
 
-add_to_path_end "$HOME/Documents/Scripts"
-add_to_path_end "$HOME/Documents/Scripts/thirdparty"
-add_to_path_end "$HOME/Scripts"
-add_to_path_end "$HOME/Scripts/thirdparty"
-add_to_path_end "$HOME/Library/Python/2.7/bin"
-add_to_path_end "$HOME/.gem/ruby/2.1.0/bin"
-add_to_path_end "$HOME/.gem/ruby/2.0.0/bin"
-add_to_path_end "$HOME/.gem/ruby/1.8/bin"
-add_to_path_end "$HOME/.rbenv/bin"
-add_to_path_end "$HOME/.cabal/bin"
-add_to_path_end "$HOME/Applications/SublimeText2"
-add_to_path_end "/c/Program Files/Sublime Text 2"
-add_to_path_end "/Applications/GitX.app/Contents/Resources"
-add_to_path_end "/Applications/TextMate.app/Contents/Resources"
-add_to_path_end "/Applications/GitHub.app/Contents/MacOS"
-add_to_path_end "/data/github/shell/bin"
 add_to_path_start "/usr/local/bin"
 add_to_path_start "/usr/local/sbin"
-add_to_path_start "$HOME/Homebrew/bin"
-add_to_path_start "$HOME/Homebrew/sbin"
+
+eval "$(hub alias -s)"
+eval "$(pyenv init -)"
+eval "$(rbenv init -)"
+eval "$(nodenv init -)"
 
 # Run rbenv if it exists
 quiet_which rbenv && add_to_path_start "$(rbenv root)/shims"
@@ -89,48 +76,7 @@ alias z="zeus"
 alias zt="zeus test"
 
 # Platform-specific stuff
-if quiet_which brew
-then
-  export BINTRAY_USER="$(git config bintray.username)"
-  export BREW_PREFIX=$(brew --prefix)
-  export BREW_REPO=$(brew --repo)
-  export HOMEBREW_DEVELOPER=1
-  export HOMEBREW_ANALYTICS=1
-  export HOMEBREW_AUTO_UPDATE=1
-  export HOMEBREW_FORCE_VENDOR_RUBY=1
-
-  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-  if [ "$USER" = "brewadmin" ]
-  then
-    export HOMEBREW_CASK_OPTS="$HOMEBREW_CASK_OPTS --binarydir=$BREW_PREFIX/bin"
-  fi
-
-  alias hbc="cd $BREW_REPO/Library/Taps/homebrew/homebrew-core"
-  alias hbv="cd $BREW_REPO/Library/Taps/homebrew/homebrew-versions"
-
-  # Output whether the dependencies for a Homebrew package are bottled.
-  brew_bottled_deps() {
-    for DEP in "$@"; do
-      echo "$DEP deps:"
-      brew deps $DEP | xargs brew info | grep stable
-      [ "$#" -ne 1 ] && echo
-    done
-  }
-
-  # Output the most popular unbottled Homebrew packages
-  brew_popular_unbottled() {
-    brew deps --all |
-      awk '{ gsub(":? ", "\n") } 1' |
-      sort |
-      uniq -c |
-      sort |
-      tail -n 500 |
-      awk '{print $2}' |
-      xargs brew info |
-      grep stable |
-      grep -v bottled
-  }
-fi
+quiet_which brew && export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 if [ $OSX ]
 then
@@ -156,49 +102,6 @@ then
 
   # Old default Curl is broken for Git on Leopard.
   [ "$OSTYPE" = "darwin9.0" ] && export GIT_SSL_NO_VERIFY=1
-elif [ $LINUX ]
-then
-  quiet_which keychain && eval `keychain -q --eval --agents ssh id_rsa`
-
-  alias su="/bin/su -"
-  alias ls="ls -F --color=auto"
-  alias open="xdg-open"
-elif [ $WINDOWS ]
-then
-  quiet_which plink && alias ssh="plink -l $(git config shell.username)"
-
-  alias ls="ls -F --color=auto"
-
-  open() {
-    cmd /C"$@"
-  }
-fi
-
-# Set up editor
-if [ -n "${SSH_CONNECTION}" ] && quiet_which rmate
-then
-  export EDITOR="rmate"
-  export GIT_EDITOR="$EDITOR -w"
-  export SVN_EDITOR=$GIT_EDITOR
-elif quiet_which mate
-then
-  export EDITOR="mate"
-  export GIT_EDITOR="$EDITOR -w"
-  export SVN_EDITOR="$GIT_EDITOR"
-elif quiet_which subl || quiet_which sublime_text
-then
-  quiet_which subl && export EDITOR="subl"
-  quiet_which sublime_text && export EDITOR="sublime_text" \
-    && alias subl="sublime_text"
-
-  export GIT_EDITOR="$EDITOR -w"
-  export SVN_EDITOR="$GIT_EDITOR"
-elif quiet_which vim
-then
-  export EDITOR="vim"
-elif quiet_which vi
-then
-  export EDITOR="vi"
 fi
 
 # Run dircolors if it exists
@@ -214,18 +117,6 @@ cd() {
     && set_terminal_app_pwd
   pwd > "$HOME/.lastpwd"
   ls
-}
-
-# Use ruby-prof to generate a call stack
-ruby-call-stack() {
-  ruby-prof --printer=call_stack --file=call_stack.html -- "$@"
-}
-
-# Use ruby-prof to generate a call stack
-rails-clean-migrate-branch() {
-  [ -n "$1" ] || return
-  git checkout master && git pull --rebase && rake db:setup db:migrate &&
-    git checkout -f "$1" && rake db:migrate
 }
 
 # Look in ./bin but do it last to avoid weird `which` results.
