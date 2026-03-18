48e3db1db3566abc3a321bdc2abbfc244564cf7a
Al Johri <al.johri@gmail.com>
2026-03-17 20:16:20 -0400

    add gwt prune

--- fish/.config/fish/functions/gwt.fish
+++ fish/.config/fish/functions/gwt.fish
@@ -1,6 +1,8 @@
 function gwt
     if test (count $argv) -eq 0
         command gwt
+    else if test "$argv[1]" = "prune"
+        command gwt prune
     else
         set dir (command gwt $argv)
         and builtin cd $dir
